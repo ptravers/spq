a4120296c16fd4870b09822ecaf24d3c13000dc2
Peter Travers <traverspw@gmail.com>
2019-11-05 07:14:02 -0500

    Add balancing features regardless of location in heirarchy

--- queue/src/lib.rs
+++ queue/src/lib.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
@@ -39,82 +38,77 @@ fn create_hash(features: &Vec<FeatureValue>, include_value: bool) -> u64 {
     return hasher.finish();
 }
 
-#[derive(Eq, Debug, Clone)]
+#[derive(Debug, Clone)]
 struct FeatureNodeValue {
-    feature_value: usize,
+    value: usize,
     items_at_index: usize,
     index: u64,
-    last_used_step: usize,
+    hash: u64,
 }
 
-impl Ord for FeatureNodeValue {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.last_used_step.cmp(&self.last_used_step)
-    }
-}
+impl FeatureNodeValue {
+    pub fn new(name: &String, value: usize, items_at_index: usize, index: u64) -> FeatureNodeValue {
+        let mut hasher = DefaultHasher::new();
 
-impl PartialOrd for FeatureNodeValue {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+        value.hash(&mut hasher);
+        name.hash(&mut hasher);
 
-impl PartialEq for FeatureNodeValue {
-    fn eq(&self, other: &Self) -> bool {
-        self.last_used_step == other.last_used_step
+        FeatureNodeValue {
+            value,
+            items_at_index,
+            index,
+            hash: hasher.finish(),
+        }
     }
 }
 
 #[derive(Debug, Clone)]
 struct FeatureNode {
+    name: String,
     values: Vec<FeatureNodeValue>,
     has_leaves: bool,
 }
 
 impl FeatureNode {
-    fn find_next(&self) -> Option<usize> {
-        let mut next: Option<usize> = None;
+    fn find_next(
+        &self,
+        feature_value_to_step: &HashMap<u64, usize>,
+        current_step: &usize,
+    ) -> Option<(usize, u64)> {
+        let mut next: Option<(usize, u64)> = None;
+        let mut lowest_step: &usize = current_step;
 
-        match self
-            .values
-            .first()
-            .map(|head_value| head_value.last_used_step)
-        {
-            Some(mut lowest_step) => {
-                for (i, value) in self.values.iter().enumerate() {
-                    if value.last_used_step <= lowest_step && value.items_at_index > 0 {
-                        next = Some(i);
-                        lowest_step = value.last_used_step;
-                    }
-                }
+        for (i, value) in self.values.iter().enumerate() {
+            let value_last_used_step = feature_value_to_step.get(&value.hash).unwrap();
+
+            if value_last_used_step <= lowest_step && value.items_at_index > 0 {
+                next = Some((i, value.hash));
+                lowest_step = value_last_used_step;
             }
-            None => {}
         }
 
         return next;
     }
 
-    pub fn peek(&self) -> Option<&FeatureNodeValue> {
-        self.find_next()
-            .and_then(|next_index| self.values.get(next_index))
+    pub fn peek(&self, feature_value_to_step: &HashMap<u64, usize>, current_step: &usize) -> Option<&FeatureNodeValue> {
+        self.find_next(feature_value_to_step, current_step)
+            .and_then(|(next_index, _)| self.values.get(next_index))
     }
 
-    pub fn peek_and_update(&mut self, step: usize) -> Option<FeatureNodeValue> {
-        let maybe_next_index = self.find_next();
-        let next_feature_node_value =
-            maybe_next_index.and_then(|next_index| self.values.get_mut(next_index));
-
-        match next_feature_node_value {
-            Some(feature_node_value) => {
-                feature_node_value.last_used_step = step;
-                feature_node_value.items_at_index -= 1;
-            }
-            None => {}
-        }
-
-        return maybe_next_index
-            .and_then(|next_index| self.values.get(next_index))
-            .map(|value| value.to_owned());
+    pub fn peek_and_update(
+        &mut self,
+        current_step: &usize,
+        feature_value_to_step: &mut HashMap<u64, usize>,
+    ) -> Option<FeatureNodeValue> {
+        self.find_next(feature_value_to_step, current_step)
+            .and_then(|(next_index, next_value_hash)| {
+                self.values.get_mut(next_index).map(|next_node_value| {
+                    feature_value_to_step.insert(next_value_hash, current_step.clone());
+                    next_node_value.items_at_index -= 1;
+                    next_node_value
+                })
+            })
+            .map(|value| value.to_owned())
     }
 }
 
@@ -125,6 +119,7 @@ struct FeatureSpace {
     dimension: usize,
     feature_names_hash: u64,
     feature_tree: HashMap<u64, FeatureNode>,
+    feature_value_to_step: HashMap<u64, usize>,
 }
 
 impl FeatureSpace {
@@ -144,36 +139,31 @@ impl FeatureSpace {
             dimension: features.len(),
             feature_names_hash,
             feature_tree: HashMap::new(),
+            feature_value_to_step: HashMap::new(),
         }
     }
 
     pub fn peek_next_leaf_feature(&self) -> Option<u64> {
-        let mut next_leaf: Option<u64> = None;
+        let mut next_node = self.root_index;
 
-        fn get_leaf(
-            feature_tree: &HashMap<u64, FeatureNode>,
-            feature_node: &FeatureNode,
-        ) -> Option<u64> {
-            match feature_node.peek() {
-                Some(feature_node_value) => {
-                    if feature_node.has_leaves {
-                        Some(feature_node_value.index)
-                    } else {
-                        feature_tree
-                            .get(&feature_node_value.index)
-                            .and_then(move |node| get_leaf(feature_tree, node))
+        for _ in 0..self.dimension {
+            match self.feature_tree.get(&next_node) {
+                Some(ref mut feature_node) if feature_node.has_leaves => {
+                    let next_feature_node_value = feature_node.peek(&self.feature_value_to_step, &self.step);
+                    return next_feature_node_value.map(|node_value| node_value.index);
+                }
+                Some(feature_node) => {
+                    let next_feature_node_value = feature_node.peek(&self.feature_value_to_step, &self.step);
+                    match next_feature_node_value.map(|node_value| node_value.index) {
+                        Some(next_index) => next_node = next_index,
+                        None => return None,
                     }
                 }
-                None => None,
+                None => {}
             }
         }
 
-        match self.feature_tree.get(&self.root_index) {
-            Some(feature_node) => next_leaf = get_leaf(&self.feature_tree, feature_node),
-            None => {}
-        }
-
-        return next_leaf.map(|leaf| leaf.to_owned());
+        return None;
     }
 
     pub fn use_next_leaf_feature(&mut self) -> Option<u64> {
@@ -183,14 +173,17 @@ impl FeatureSpace {
         for _ in 0..self.dimension {
             match self.feature_tree.get_mut(&next_node) {
                 Some(ref mut feature_node) if feature_node.has_leaves => {
-                    let next_feature_node_value = feature_node.peek_and_update(self.step);
-                   return next_feature_node_value.map(|node_value| node_value.index);
+                    let next_feature_node_value =
+                        feature_node.peek_and_update(&self.step, &mut self.feature_value_to_step);
+                    return next_feature_node_value.map(|node_value| node_value.index);
                 }
                 Some(feature_node) => {
-                    let next_feature_node_value = feature_node.peek_and_update(self.step);
+                    let next_feature_node_value =
+                        feature_node.peek_and_update(&self.step, &mut self.feature_value_to_step);
+
                     match next_feature_node_value.map(|node_value| node_value.index) {
                         Some(next_index) => next_node = next_index,
-                        None => return None
+                        None => return None,
                     }
                 }
                 None => {}
@@ -204,27 +197,29 @@ impl FeatureSpace {
         self.total_items += 1;
         self.step += 1;
 
-        let mut previous_hash = leaf_index;
+        let mut child_index = leaf_index;
         let mut i = 1;
         let currently_empty = self.feature_tree.is_empty();
 
-        let mut hash_to_check;
+        let mut current_node_index;
 
         features.reverse();
 
         for feature in features.clone().iter() {
             let mut next_features = features.clone();
+            next_features.reverse();
             next_features.truncate(i);
-            hash_to_check = create_hash(&next_features, false);
+            current_node_index = create_hash(&next_features, i != features.len());
 
-            match self.feature_tree.get_mut(&hash_to_check) {
-                Some(FeatureNode {
+            match self.feature_tree.get_mut(&current_node_index) {
+                Some(FeatureNode{
+                    name: _,
                     values,
                     has_leaves: _,
                 }) => {
-                    let maybe_value = values.iter_mut().find(|feature_values| {
-                        feature_values.feature_value == feature.value
-                            && feature_values.index == previous_hash
+                    let maybe_value = values.iter_mut().find(|feature_node_value| {
+                        feature_node_value.value == feature.value
+                            && feature_node_value.index == child_index
                     });
 
                     match maybe_value {
@@ -232,44 +227,49 @@ impl FeatureSpace {
                             value.items_at_index += 1;
                         }
                         None => {
-                            values.push(FeatureNodeValue {
-                                feature_value: feature.value,
-                                items_at_index: 1,
-                                index: previous_hash,
-                                last_used_step: self.step,
-                            });
+                            let value = FeatureNodeValue::new(
+                                &feature.name,
+                                feature.value,
+                                1,
+                                child_index,
+                            );
 
-                            self.step += 1;
+                            values.push(value.clone());
+
+                            if !self.feature_value_to_step.contains_key(&value.hash) {
+                                self.feature_value_to_step.insert(value.hash, self.step);
+                            }
                         }
                     }
                 }
                 None => {
                     let mut values = Vec::new();
 
-                    values.push(FeatureNodeValue {
-                        feature_value: feature.value,
-                        items_at_index: 1,
-                        index: previous_hash,
-                        last_used_step: self.step,
-                    });
+                    let value =
+                        FeatureNodeValue::new(&feature.name, feature.value, 1, child_index);
+
+                    values.push(value.clone());
+
+                    if !self.feature_value_to_step.contains_key(&value.hash) {
+                        self.feature_value_to_step.insert(value.hash, self.step);
+                    }
 
                     self.feature_tree.insert(
-                        hash_to_check,
+                        current_node_index,
                         FeatureNode {
+                            name: feature.name.clone(),
                             values: values,
                             has_leaves: i == 1,
                         },
                     );
 
                     if currently_empty && i == features.len() {
-                        self.root_index = hash_to_check;
+                        self.root_index = current_node_index;
                     }
-
-                    self.step += 1;
                 }
             }
 
-            previous_hash = hash_to_check;
+            child_index = current_node_index;
             i += 1;
         }
     }
@@ -299,7 +299,9 @@ impl<T: Clone + Debug + Copy + Ord> SortingPriorityQueue<T> {
             let feature_names_hash = create_hash(&features, false);
 
             if feature_names_hash != self.feature_space.feature_names_hash {
-                return Err("Invalid feature vector must have same feature names as initialization");
+                return Err(
+                    "Invalid feature vector must have same feature names as initialization",
+                );
             }
 
             let hash = create_hash(&features, true);
@@ -356,6 +358,7 @@ mod tests {
     use super::*;
 
     static LEAF_FEATURE_NAME: &str = "leaf";
+    static ROOT_FEATURE_NAME: &str = "root";
     lazy_static! {
         static ref DEFAULT_FEATURES: Vec<FeatureValue> =
             vec![FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1)];
@@ -469,9 +472,8 @@ mod tests {
 
     #[test]
     fn must_balance_selection_by_feature_heirarchy() {
-        let root_feature_name: String = "root".to_string();
         let feature_names: Vec<String> =
-            vec![root_feature_name.clone(), LEAF_FEATURE_NAME.to_string()];
+            vec![ROOT_FEATURE_NAME.to_string(), LEAF_FEATURE_NAME.to_string()];
 
         let mut queue = SortingPriorityQueue::<i32>::new(feature_names);
 
@@ -483,7 +485,7 @@ mod tests {
             .add(
                 first_item,
                 vec![
-                    FeatureValue::new(root_feature_name.clone(), 1),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -492,7 +494,7 @@ mod tests {
             .add(
                 unseen_item,
                 vec![
-                    FeatureValue::new(root_feature_name.clone(), 1),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -501,7 +503,7 @@ mod tests {
             .add(
                 fairest_item,
                 vec![
-                    FeatureValue::new(root_feature_name.to_string(), 2),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 2),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -514,9 +516,8 @@ mod tests {
 
     #[test]
     fn should_be_drained_by_feature_heirarchy() {
-        let root_feature_name: String = "root".to_string();
         let feature_names: Vec<String> =
-            vec![root_feature_name.clone(), LEAF_FEATURE_NAME.to_string()];
+            vec![ROOT_FEATURE_NAME.to_string(), LEAF_FEATURE_NAME.to_string()];
 
         let mut queue = SortingPriorityQueue::<i32>::new(feature_names);
 
@@ -529,7 +530,7 @@ mod tests {
             .add(
                 first_item,
                 vec![
-                    FeatureValue::new(root_feature_name.clone(), 1),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -538,7 +539,7 @@ mod tests {
             .add(
                 second_last_item,
                 vec![
-                    FeatureValue::new(root_feature_name.clone(), 1),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -547,7 +548,7 @@ mod tests {
             .add(
                 last_item,
                 vec![
-                    FeatureValue::new(root_feature_name.clone(), 1),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -556,7 +557,7 @@ mod tests {
             .add(
                 fairest_item,
                 vec![
-                    FeatureValue::new(root_feature_name.to_string(), 2),
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 2),
                     FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
                 ],
             )
@@ -589,5 +590,51 @@ mod tests {
         let result = queue.add(1, DEFAULT_FEATURES.clone());
 
         assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn must_guarantee_fair_retrieval_by_feature_value_regardless_of_path() {
+        let feature_names: Vec<String> =
+            vec![ROOT_FEATURE_NAME.to_string(), LEAF_FEATURE_NAME.to_string()];
+
+        let mut queue = SortingPriorityQueue::<i32>::new(feature_names);
+
+        let first_item = 3;
+        let last_item = 2;
+        let fairest_item = 1;
+
+        queue
+            .add(
+                first_item,
+                vec![
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 1),
+                    FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
+                ],
+            )
+            .unwrap();
+        queue
+            .add(
+                last_item,
+                vec![
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 2),
+                    FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 1),
+                ],
+            )
+            .unwrap();
+        queue
+            .add(
+                fairest_item,
+                vec![
+                    FeatureValue::new(ROOT_FEATURE_NAME.to_string(), 2),
+                    FeatureValue::new(LEAF_FEATURE_NAME.to_string(), 2),
+                ],
+            )
+            .unwrap();
+
+        assert_eq!(queue.next(), Some(first_item));
+
+        assert_eq!(queue.next(), Some(fairest_item));
+
+        assert_eq!(queue.next(), Some(last_item));
     }
 }
