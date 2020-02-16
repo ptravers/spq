c98c7164594d259636abf5e096c4342538945ac2
Peter Travers <traverspw@gmail.com>
2020-02-15 22:33:08 -0500

    Add watching health of queue

--- tests/integration_tests/test_heath.py
+++ tests/integration_tests/test_heath.py
@@ -4,3 +4,14 @@ from proto import spq_pb2
 def test_check_health(health_client):
     response = health_client.Check(spq_pb2.HealthCheckRequest())
     assert response.status == 1
+
+
+def test_watch_health(health_client):
+    total_events = 0
+
+    for response in health_client.Watch(spq_pb2.HealthCheckRequest()):
+        total_events += 1
+        assert response.status == 1
+
+        if total_events >= 2:
+            break
